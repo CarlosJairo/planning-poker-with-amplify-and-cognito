26dc8cb4d9120d7f858ad3f7f7f90a053d6437f0
CarlosJairo <carlosjairo38@gmail.com>
2024-08-11 18:43:01 -0500

    agregando amplify y solucionando error de aws-exports.js

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "planningpokertsda374d4b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
