a5a2a872cce270e355425b116509977198613606
CarlosJairo <carlosjairo38@gmail.com>
2024-08-11 20:45:17 -0500

    agregando UI de amplify y estilos de root

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,43 @@ import { useState } from "react";
 import SplashScreen from "./components/pages/SplashScreen/SplashScreen";
 import CreateGameScreen from "./components/pages/CreateGameScreen/CreateGameScreen";
 import GameTableScreen from "./components/pages/GameTableScreen/GameTableScreen";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
+
+// Estilos
+import "@aws-amplify/ui-react/styles.css";
 
 function App() {
   const [showSplashScreen, setShowSplashScreen] = useState<boolean>(true);
 
   return (
-    <>
-      <HashRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              showSplashScreen ? (
-                <SplashScreen
-                  showSplashScreen={showSplashScreen}
-                  setShowSplashScreen={setShowSplashScreen}
-                />
-              ) : (
-                <CreateGameScreen />
-              )
-            }
-          />
-          <Route path="/game/:gameName" element={<GameTableScreen />} />
-        </Routes>
-      </HashRouter>
-    </>
+    <Authenticator>
+      {() => (
+        <>
+          <HashRouter>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  showSplashScreen ? (
+                    <SplashScreen
+                      showSplashScreen={showSplashScreen}
+                      setShowSplashScreen={setShowSplashScreen}
+                    />
+                  ) : (
+                    <CreateGameScreen />
+                  )
+                }
+              />
+              <Route path="/game/:gameName" element={<GameTableScreen />} />
+            </Routes>
+          </HashRouter>
+        </>
+      )}
+    </Authenticator>
   );
 }
 
